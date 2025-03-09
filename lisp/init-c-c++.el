3b59e8f7ff21726052b71f9bcb2d59f14901d164
peter159 <peter.linyi159@gmail.com>
2025-03-09 08:54:56 +0800

    fix c++ dev

--- lisp/init-c-c++.el
+++ lisp/init-c-c++.el
@@ -32,6 +32,31 @@
 
 (mark-time-here)
 
+(defun unicorn/clang-setup-project-config()
+  "setup config env for project root"
+  (interactive)
+  (shell-command "cp ~/.emacs.d/config_snippets/cpp/.clangd .")
+  )
+
+(defun generate_compile_commands ()
+  "检查当前文件所在目录是否为 build 目录，如果不是则拒绝执行；
+否则将 ~/.emacs.d/config_snippets/cpp 目录下的 .clangd 和 CMakeLists.txt 文件复制到当前文件所在目录的上一层（如果不存在），
+并在当前目录下执行 cmake 命令生成 compile_commands.json 文件。"
+  (interactive)
+  (let* ((current-dir (file-name-directory (or buffer-file-name default-directory)))
+         (dir-name (file-name-nondirectory (directory-file-name current-dir))))
+    (if (not (string= dir-name "build"))
+        (error "当前文件不在 build 目录下，无法执行 generate_compile_commands")
+      (let ((parent-dir (file-name-directory (directory-file-name current-dir))))
+        ;; 仅在目标文件不存在时复制文件到上一级目录
+        (unless (file-exists-p (expand-file-name ".clangd" parent-dir))
+          (copy-file "~/.emacs.d/config_snippets/cpp/.clangd" parent-dir t))
+        (unless (file-exists-p (expand-file-name "CMakeLists.txt" parent-dir))
+          (copy-file "~/.emacs.d/config_snippets/cpp/CMakeLists.txt" parent-dir t))
+        ;; 在当前目录下执行 cmake 命令
+        (let ((default-directory current-dir))
+          (shell-command "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .."))))))
+
 (use-package cc-mode
   :ensure t
   :init
